e69e699065bfe01ea4c6243542a06ddac75439eb
Garrett Olson <student@epicodus.com>
2015-05-01 09:40:13 -0700

    Add before block to clear words, test that not calling #save does not add word to dictionary

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -2,16 +2,20 @@ require('rspec')
 require('word')
 
 describe('Word') do
+  before do
+    Word.clear()
+  end
+
   describe('#save') do
     it('adds the word to an array containing all words in the dictionary') do
       word = Word.new({:word => "logical"})
       word.save()
       expect(Word.all()).to(eq([word]))
     end
+  end
 
-    it('does not add word if #save is not called on word object') do
-      word = Word.new({:word => "logical"})
-      expect(Word.all()).to(eq([]))
-    end
+  it('does not add the word to array containing all words if #save is not called') do
+    word = Word.new({:word => "logical"})
+    expect(Word.all()).to(eq([]))
   end
 end
