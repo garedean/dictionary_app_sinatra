424d2a30ee4c78605f8b511a38a7e295df92003f
Garrett Olson <student@epicodus.com>
2015-05-01 11:39:21 -0700

    Add route for get('words/new' and post('words') for add new words

--- app.rb
+++ app.rb
@@ -3,8 +3,21 @@ require('sinatra/reloader')
 require('./lib/word.rb')
 require('./lib/definition.rb')
 also_reload('lib/**/*.rb')
-set(:show_exceptions, false)
+#set(:show_exceptions, false)
 
 get('/') do
+  @words = Word.all()
   erb(:index)
+end
+
+get('/words/new') do
+  erb(:word_form)
+end
+
+post('/words') do
+  word = params.fetch('word')
+  word_object = Word.new({:word => word})
+  word_object.save()
+
+  redirect to('/')
 end
