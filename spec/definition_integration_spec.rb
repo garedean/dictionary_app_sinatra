cd07c79427abc7f099c1b5f1e2bed928415d7829
Garrett Olson <student@epicodus.com>
2015-05-01 11:05:41 -0700

    Test that a message appears if no words exist in dictionary

---
+++ spec/definition_integration_spec.rb
@@ -0,0 +1,21 @@
+require('capybara/rspec')
+require('./app')
+require('pry')
+Capybara.app = Sinatra::Application
+
+describe('the online dictionary app', :type => :feature) do
+  before do
+    Word.clear()
+    visit('/')
+  end
+
+  describe('/ path') do
+    it('loads the home page') do
+      expect(page).to(have_content('Online Dictionary'))
+    end
+
+    it('displays message when no words exist') do
+      expect(page).to(have_content("There aren't any words in the dictionary. Why not add one?"))
+    end
+  end
+end
