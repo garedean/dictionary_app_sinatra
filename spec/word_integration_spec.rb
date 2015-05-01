c275ea0937de7ee803ddd3bceee1422052f50785
Garrett Olson <student@epicodus.com>
2015-05-01 13:34:03 -0700

    Refactor spec

--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -10,18 +10,44 @@ describe('the online dictionary app', :type => :feature) do
   end
 
   describe('/ path') do
-    it('loads the home page') do
-      expect(page).to(have_content('Online Dictionary'))
+    it('displays message when no words exist') do
+      expect(page).to(have_content("There aren't any words in the dictionary. Add one."))
     end
 
-    it('displays message when no words exist') do
-      expect(page).to(have_content("There aren't any words in the dictionary. Why not add one?"))
+    it('displays dictionary words when words exist') do
+      click_link('Add one.')
+      fill_in('word', :with => 'epic')
+      click_button('Add Word')
+      expect(page).to(have_content("Epic"))
     end
   end
 
-  describe('/words POST') do
-    it('adds a word to the dictionary') do
+  describe('/words path') do
+    it('adds a word to the dictionary when a form is posted to it') do
       click_link('Add one.')
+      fill_in('word', :with => 'epic')
+      click_button('Add Word')
+    end
+  end
+
+  describe('/words/:id path') do
+    it('displays a message if no definitions exist for the word') do
+      click_link('Add one.')
+      fill_in('word', :with => 'epic')
+      click_button('Add Word')
+      click_link('Epic')
+      expect(page).to(have_content("There aren't any definitions for this word."))
+    end
+
+    it('displays definitions for a word when definitions exist') do
+      click_link('Add one.')
+      fill_in('word', :with => 'kitten')
+      click_button('Add Word')
+      click_link('Kitten')
+      click_link('Add one.')
+      fill_in('definition', :with => 'a young cat.')
+      click_button('Add Definition')
+      expect(page).to(have_content("Definition #1: a young cat."))
     end
   end
 end
