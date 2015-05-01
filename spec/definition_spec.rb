a3eef3cbb5077dd75be2e48fea8ff66f763950db
Garrett Olson <student@epicodus.com>
2015-05-01 10:35:24 -0700

    Test that #definition returns a string definition

---
+++ spec/definition_spec.rb
@@ -0,0 +1,13 @@
+require('rspec')
+require('definition')
+
+describe('Definition') do
+  describe('#definition') do
+    it('returns a definition') do
+      word5 = Word.new({:word => 'puppy'})
+      word5.save()
+      word5.add_definition("A young canine.")
+      expect(word5.definitions().first().definition()).to(eq("A young canine."))
+    end
+  end
+end
